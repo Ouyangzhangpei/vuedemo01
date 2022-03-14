7f2a6d45ae7875bbe5201442ec44e67c33e58307
LAPTOP-OB94FRHF\zpp <zhangpeipei95@126.com>
2022-03-14 15:55:33 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{msg}}</div>
+  <div>{{msg}}</div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      msg:'你好'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
